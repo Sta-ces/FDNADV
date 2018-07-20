94a611baab40e25beafb38ef84fb6a2bf3c396e7
Staces <staescedric@gmail.com>
2018-07-20 20:02:36 +0200

    DechetControler script

---
+++ Assets/_Project/Scripts/Controls.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Controls : MonoBehaviour {
+
+    public static float GetHorizontal
+    {
+        get {
+            float result = 0;
+            if (Input.GetButtonDown("Horizontal"))
+            {
+                if(Input.GetAxis("Horizontal") > 0)
+                {
+                    result = 1;
+                }
+
+                if(Input.GetAxis("Horizontal") < 0)
+                {
+                    result = -1;
+                }
+            }
+            return result;
+        }
+    }
+
+    public static bool LeftInput{
+        get{ return Input.GetButtonDown("Horizontal") && Input.GetAxis("Horizontal") < 0; }
+    }
+
+    public static bool RightInput {
+        get { return Input.GetButtonDown("Horizontal") && Input.GetAxis("Horizontal") > 0; }
+    }
+}
---
+++ Assets/_Project/Scripts/DechetControler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DechetControler : MonoBehaviour {
+
+    [System.Serializable]
+    public struct Limit
+    {
+        public float Left;
+        public float Right;
+    }
+    [SerializeField]
+    private Limit TrashLimit;
+	
+	void FixedUpdate () {
+        MoveDechet();
+	}
+
+    void MoveDechet()
+    {
+        Vector3 pos = transform.position;
+
+        pos.x += Controls.GetHorizontal;
+
+        pos.x = Mathf.Clamp( pos.x, TrashLimit.Left, TrashLimit.Right );
+
+        transform.position = pos;
+    }
+}
