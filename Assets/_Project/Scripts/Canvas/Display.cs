ffcfb52da23abe87c7c68be48c19fe921b1bedec
Staces <staescedric@gmail.com>
2018-07-21 17:09:14 +0200

    Score

---
+++ Assets/_Project/Scripts/Canvas/Display.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Display : MonoBehaviour {
+
+    private static float score = 0;
+    public static float Score
+    {
+        get { return score; }
+        set { score = value; }
+    }
+
+    [SerializeField]
+    private Text ScoreText;
+
+    [Header("Events")]
+    [SerializeField]
+    private UnityEvent OnScore;
+
+    public void DisplayScore()
+    {
+        ScoreText.text = Score.ToString();
+        OnScore.Invoke();
+    }
+}
---
+++ Assets/_Project/Scripts/DechetsScore.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DechetsScore : MonoBehaviour
+{
+    [Range(0, 5)]
+    public float PointScore = 1;
+
+    public void AddScore()
+    {
+        Display.Score = PointScore;
+    }
+}
